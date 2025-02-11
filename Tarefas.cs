1c8fdf9061a8b936e5401f8e62501c8aa824b392
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2025-02-11 11:17:10 -0300

    Adicionado

--- Tarefas.cs
+++ Tarefas.cs
@@ -1,11 +1,21 @@
+using System.Data.Common;
+
 namespace Tarefas
 {
     class Tarefa
     {
-        
+
             public int Id { get; set; }  
             public string Descricao { get; set; }
 
+            public bool Concluido { get ; set; }
+
+        public void Tarefa(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+            Concluido = false;
+        }
         public void ExibirTarefa()
         {
 
